dfe424d30df2e5dae6e5c17622cc169962c8d8ea
Lmt310104 <lmtoan311@gmail.com>
2024-10-14 17:07:05 +0700

    add seed data

---
+++ prisma/seeds/authors.ts
@@ -0,0 +1,45 @@
+const authors_data = [
+  {
+    name: 'John "Red" Shea',
+    birthday: new Date('1965-08-12'),
+    description:
+      'John "Red" Shea, 40, was a top lieutenant in the South Boston Irish mob run, led by James "Whitey" B...',
+  },
+  {
+    name: 'Mary Kelly',
+    birthday: new Date('1957-01-18'),
+    description:
+      'Mary Kelly was an English crime writer best known for the Inspector Brett Nightingale series. Writin...',
+  },
+  {
+    name: 'Lucie Dufresne',
+    birthday: new Date('1858-01-17'),
+    description:
+      'Lucie Dufresne was born in 1951 in Trois-Rivières between two rivers: the St-Lawrence and the Saint-...',
+  },
+  {
+    name: 'Gilles Pimparé',
+    birthday: new Date('1978-09-22'),
+    description:
+      'Gilles Pimparé is a Canadian author, born in Montreal in 1978. He is the author of the novels "Les co...',
+  },
+  {
+    name: 'Lise Tremblay',
+    birthday: new Date('1957-01-18'),
+    description:
+      'Lise Tremblay was born in 1957 in Chicoutimi, Quebec. She is the author of several novels and short s...',
+  },
+  {
+    name: 'Ramón González Férriz',
+    birthday: new Date('1963-11-18'),
+    description:
+      'Ramón González Férriz was born in Madrid in 1963. He is a writer, journalist and translator. He has p...',
+  },
+  {
+    name: 'Maja Ilisch',
+    birthday: new Date('1971-03-25'),
+    description:
+      'Maja Ilisch was born in 1971 in Berlin. She studied German, English and American literature and philo...',
+  },
+];
+export default authors_data;
