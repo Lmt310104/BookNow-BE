9f4d7e847281e3523503aafacadb68da382628df
Lmt310104 <lmtoan311@gmail.com>
2024-10-25 11:04:10 +0700

    fix: refresh_token guard

--- src/common/guards/refreshtoken.guard.ts
+++ src/common/guards/refreshtoken.guard.ts
@@ -3,7 +3,6 @@ import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 import { JWT_REFRESH_STRATEGY } from 'src/utils/constants';
-import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 export class RefreshTokenGuard extends AuthGuard(JWT_REFRESH_STRATEGY) {
   constructor(private readonly reflector: Reflector) {
@@ -12,13 +11,6 @@ export class RefreshTokenGuard extends AuthGuard(JWT_REFRESH_STRATEGY) {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const isPublic = this.reflector.get<boolean>(
-      IS_PUBLIC_KEY,
-      context.getHandler(),
-    );
-    if (isPublic) {
-      return true;
-    }
     return super.canActivate(context);
   }
 }
