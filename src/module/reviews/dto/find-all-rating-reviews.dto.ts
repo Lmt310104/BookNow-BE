28a5e9f18e418b241124529e26b7c9e42f443012
Lmt310104 <lmtoan311@gmail.com>
2024-11-06 09:40:30 +0700

    fix: reviews

--- src/module/reviews/dto/find-all-rating-reviews.dto.ts
+++ src/module/reviews/dto/find-all-rating-reviews.dto.ts
@@ -1,3 +1,18 @@
+import { ReviewState } from '@prisma/client';
+import { IsArray, IsEnum, IsOptional, IsString } from 'class-validator';
 import { PageOptionsDto } from 'src/utils/page-options-dto';
 
-export class GetReviewsDto extends PageOptionsDto {}
+export class GetReviewsDto extends PageOptionsDto {
+  @IsString()
+  @IsOptional()
+  search?: string;
+  @IsArray()
+  @IsOptional()
+  rating?: number[];
+  @IsString()
+  @IsOptional()
+  date?: string;
+  @IsEnum(ReviewState)
+  @IsOptional()
+  state?: ReviewState;
+}
