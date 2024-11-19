3898a2f5f1a122283fc7faa12f5ed2ec66274e1a
Lmt310104 <lmtoan311@gmail.com>
2024-11-19 16:52:47 +0700

    feat: add websocket

--- src/module/events_gateway/event_gateway.module.ts
+++ src/module/events_gateway/event_gateway.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './event_gateway.gateway';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [EventsGateway],
 })
 export class EventsModule {}
